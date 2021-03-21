b05f37b840555eaa17dd3b07e1175753f86e85cc
Ricky Shi <ricky.shiyouping@gmail.com>
2021-03-21 17:59:16 +0800

    feat: first commit for the backend project

---
+++ server/src/test/java/com/hohomalls/ReactiveApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hohomalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
