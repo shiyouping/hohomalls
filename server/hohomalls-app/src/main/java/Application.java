57fd1ba893cb288ad26b9547bea80c62cb619302
Ricky Shi <ricky.shiyouping@gmail.com>
2021-04-11 15:31:23 +0800

    feat: remove buildSrc

---
+++ server/hohomalls-app/src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main entry of this application.
+ *
+ * @author ricky.shiyouping@gmail.com
+ * @since 11/4/2021
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {}
+}
