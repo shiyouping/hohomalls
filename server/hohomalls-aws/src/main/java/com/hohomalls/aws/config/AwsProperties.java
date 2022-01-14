33eb5fc438b7ec06b6e6918fe961793b111118f0
Ricky Shi <ricky.shiyouping@gmail.com>
2022-01-14 19:24:01 +0800

    feat: add basic s3 configuration

---
+++ server/hohomalls-aws/src/main/java/com/hohomalls/aws/config/AwsProperties.java
@@ -0,0 +1,24 @@
+package com.hohomalls.aws.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import static com.hohomalls.core.common.Global.CONFIG_PROPERTY_PREFIX;
+
+/**
+ * AwsProperties.
+ *
+ * @author ricky.shiyouping@gmail.com
+ * @since 14/1/2022
+ */
+@Validated
+@ConfigurationProperties(prefix = CONFIG_PROPERTY_PREFIX + "aws")
+public record AwsProperties(@NotNull S3 s3) {
+
+  @Validated
+  public record S3(@NotEmpty String bucketName) {
+  }
+}
--- server/hohomalls-aws/src/main/java/com/hohomalls/aws/service/S3ServiceImpl.java
+++ server/hohomalls-aws/src/main/java/com/hohomalls/aws/service/S3ServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hohomalls.aws.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.hohomalls.aws.config.AwsProperties;
 import com.hohomalls.core.service.StorageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * S3ServiceImpl.
@@ -13,7 +18,19 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class S3ServiceImpl implements StorageService {
+
+  private final AwsProperties awsProperties;
+  private final AmazonS3Client amazonS3Client;
+
+  private String bucketUrl;
+
+  @PostConstruct
+  public void postConstruct() {
+    var bucketName = this.awsProperties.s3().bucketName();
+    this.bucketUrl = "https://%s.s3.amazonaws.com".formatted(bucketName);
+  }
 
   @Override
   public void save(byte[] data, @NotNull String filePath, @NotNull String fileName) {}
