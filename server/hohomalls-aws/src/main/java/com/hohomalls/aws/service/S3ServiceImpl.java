89ff60f772baee1a50da091c323d0c0bec90dff4
Ricky Shi <ricky.shiyouping@gmail.com>
2022-01-25 21:42:01 +0800

    feat: finish s3 service with pass tests

--- server/hohomalls-aws/src/main/java/com/hohomalls/aws/service/S3ServiceImpl.java
+++ server/hohomalls-aws/src/main/java/com/hohomalls/aws/service/S3ServiceImpl.java
@@ -2,15 +2,17 @@ package com.hohomalls.aws.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PublicAccessBlockConfiguration;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.SetPublicAccessBlockRequest;
 import com.hohomalls.aws.config.AwsProperties;
 import com.hohomalls.core.common.Constant;
 import com.hohomalls.core.service.StorageService;
+import com.hohomalls.core.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
@@ -27,17 +29,22 @@ import java.io.ByteArrayInputStream;
 @RequiredArgsConstructor
 public class S3ServiceImpl implements StorageService {
 
+  private static final String BUCKET_POLICY_TEMPLATE = "s3-bucket-policy-template.json";
   private final AwsProperties awsProperties;
   private final AmazonS3Client amazonS3Client;
 
   @PostConstruct
   public void initS3Bucket() {
     var bucketName = this.awsProperties.s3().bucketName();
-    var buckets = this.amazonS3Client.listBuckets();
+    try {
+      if (this.amazonS3Client.doesBucketExistV2(bucketName)) {
+        return;
+      }
 
-    if (CollectionUtils.isEmpty(buckets)
-        || buckets.stream().noneMatch(bucket -> bucketName.equals(bucket.getName()))) {
-      this.amazonS3Client.createBucket(bucketName);
+      this.createBucket(bucketName);
+    } catch (Exception e) {
+      S3ServiceImpl.log.error("Failed to create the S3 bucket %s".formatted(bucketName), e);
+      throw e; // Fail fast
     }
   }
 
@@ -45,16 +52,53 @@ public class S3ServiceImpl implements StorageService {
   public Mono<Void> save(byte[] data, @NotNull String filePath, @NotNull String fileName) {
     return Mono.fromRunnable(
         () -> {
-          var key = String.join(Constant.SIGN_SLASH, filePath, fileName);
           var bucketName = this.awsProperties.s3().bucketName();
-          S3ServiceImpl.log.info("Uploading a file to S3. BucketName={}, Key={}", bucketName, key);
-          this.amazonS3Client.putObject(this.createPutObjectRequest(data, key, bucketName));
+          S3ServiceImpl.log.info(
+              "Uploading a file to S3. BucketName={}, filePath={}, fileName={}",
+              bucketName,
+              filePath,
+              fileName);
+
+          var key = String.join(Constant.SIGN_SLASH, filePath, fileName);
+          if (this.amazonS3Client.doesObjectExist(bucketName, key)) {
+            return;
+          }
+
+          this.uploadFile(data, key, bucketName);
         });
   }
 
-  private PutObjectRequest createPutObjectRequest(
-      byte[] data, @NotNull String key, String bucketName) {
-    return new PutObjectRequest(
-        bucketName, key, new ByteArrayInputStream(data), new ObjectMetadata());
+  private void createBucket(String bucketName) {
+    S3ServiceImpl.log.info("Creating a S3 bucket with name={}", bucketName);
+
+    // Create a bucket with public access
+    this.amazonS3Client.createBucket(bucketName);
+
+    // Set the policy to allow public read
+    var policyText =
+        FileUtil.readAsString(S3ServiceImpl.BUCKET_POLICY_TEMPLATE).formatted(bucketName);
+    this.amazonS3Client.setBucketPolicy(bucketName, policyText);
+
+    // Disable public access
+    var publicAccessConfig =
+        new PublicAccessBlockConfiguration()
+            .withBlockPublicAcls(true)
+            .withIgnorePublicAcls(true)
+            .withBlockPublicPolicy(true)
+            // Don't enable the following option
+            .withRestrictPublicBuckets(false);
+    this.amazonS3Client.setPublicAccessBlock(
+        new SetPublicAccessBlockRequest()
+            .withBucketName(bucketName)
+            .withPublicAccessBlockConfiguration(publicAccessConfig));
+  }
+
+  private void uploadFile(byte[] data, String key, String bucketName) {
+    // For big files, should use multipart upload; otherwise, there will be a memory leakage
+    // See an example at:
+    // https://medium.com/digio-australia/multipart-upload-to-s3-using-aws-sdk-for-java-d3fd2e17f515
+    var request =
+        new PutObjectRequest(bucketName, key, new ByteArrayInputStream(data), new ObjectMetadata());
+    this.amazonS3Client.putObject(request);
   }
 }
